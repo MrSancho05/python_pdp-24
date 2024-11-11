e3a14c2402f73587a5d4331313727b0ffe13d0d5
MrSancho05 <bahrombekovsanjarbek@gmail.com>
2024-11-11 12:04:30 +0500

    2nd hour of classwork

--- python15/classwork/main.py
+++ python15/classwork/main.py
@@ -1,3 +1,4 @@
+# DARSDA YAXSHI ISTIROK ETISHIM VA YAXSHI O'ZLASHTIRA OLISHIM ALLOH GA OSON
 # num = int(input('Enter number: '))
 # qoldiq = ''
 
@@ -6,14 +7,87 @@
 #     num //= 2
 # print(qoldiq)
 
-num = int(input('num: '))
-summa = ''
-while num >= 1:
-    qoldiq = num % 16
-    if qoldiq > 9:
-        qoldiq += 55
-        qoldiq = chr(qoldiq)
-    summa = str(qoldiq) + summa
-    num //= 16
-print(summa)
+# num = int(input('num: '))
+# summa = ''
+# while num >= 1:
+#     qoldiq = num % 16
+#     if qoldiq > 9:
+#         qoldiq += 55
+#         qoldiq = chr(qoldiq)
+#     summa = str(qoldiq) + summa
+#     num //= 16
+# print(summa)
+
+# sum = 0
+# if num < 0: num *= -1
+# while num > 0:
+#     b = num % 10
+#     sum += b
+#     num //= 10
+
+# print(sum)  
+# 123456
+
+# i = 65
+# while i <= num:
+#     if i % 5 == 0:
+#         print()
+#     print(chr(i), end= ' ')
+#     i += 1
+
+
+
+# FOR 
+# fruits = ['apple', 'banan', 'mango', 'kiwi', 'pineapple']
+# for fruit in fruits:
+#     print(fruit)
+
+# word = 'Hello Sancho'
+# for letter in word:
+#     print(letter)
+
+# for x in range(7):
+#     print(x)
+
+# correct
+# for x in range(1,7):
+#     print(x)
+
+# # incorrect
+# for x in range(10, 7):
+#     print(x)
+
+# # 3 taga ishirish
+# for i in range(9, 90, 9):
+#     print(i)
+
+
+# task 1
+# sum = 0
+# for i in range(1,101):
+#     # print(i)
+#     sum += i
+
+# print(sum)
+
+# task 2
+# jSum = 0
+# tSum = 0
+# for i in range(1, 51):
+#     if i % 2 == 0:
+#         jSum += i
+#         # print(f'juft-{i}')
+#     else: 
+#         tSum += i
+#         # print(f"toq-{i}")
+# print(jSum)
+# print(tSum)
+
+# TASK 3
+for i in range(-80, 81):
+    print(i)
+
+
+
+
 
